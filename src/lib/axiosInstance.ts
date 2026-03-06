58f736107716c00e09733408f46b42844a091112
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2026-03-07 00:19:10 +0500

    Switched to localStorage

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,6 +1,7 @@
 import axios, { type AxiosInstance } from "axios";
 
 import { useUserStore } from "@/store/userStore";
+import { getToken } from "@/utils/localStorage";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -9,7 +10,13 @@ const axiosInstance: AxiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use(
-  (config) => config,
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
   (error) => Promise.reject(error)
 );
 
